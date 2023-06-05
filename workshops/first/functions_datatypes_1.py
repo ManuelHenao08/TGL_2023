5f91dcc2e724dc8b7e70a78c2f0a05bf8fa3b838
Manuel Henao <manuel.henao9913@gmail.com>
2023-06-05 13:23:05 -0500

    exercises 1-2 added about functions and datatypes

---
+++ workshops/first/functions_datatypes_1.py
@@ -0,0 +1,65 @@
+def find_maximum(numbers:list):
+    """
+    Finds the maximum value in a list of numbers.
+
+    Args:
+        numbers (list): A list of numbers.
+
+    Returns:
+        int or float: The maximum value in the list.
+
+    Raises:
+        ValueError: If the input list is empty.
+    """
+    if len(numbers) == 0:
+        raise ValueError("Input list cannot be empty")
+
+    maximum = numbers[0]  # Initialize maximum with the first element
+
+    for num in numbers:
+        if num > maximum:
+            maximum = num
+
+    return maximum
+
+
+def main():
+    try:
+        numbers=[]
+        flag = False
+        while not flag:
+            print("-------")
+            print("Welcome Main Menu")
+            print("Find the maximun number on a list -> (1)")
+            print("Exit -> (2)")
+            user_input = input("Please choose a mode > ")
+
+            if user_input == "1":  # Find the maximun number on a list
+                
+                while True:
+                    try:
+                        user_input = input("Enter a number (or 'x' to finish): ")
+                        
+                        if user_input.lower() == 'x':
+                            break
+                        
+                        number = float(user_input)
+                        numbers.append(number)
+                    
+                    except ValueError:
+                        print("Invalid input! Please enter a valid number or 'x' to finish.")
+
+                print(f"For the list {numbers} the maximum value is {find_maximum(numbers)}")
+
+            elif user_input == "2": #Exit
+                flag = True
+
+    except KeyboardInterrupt as error :
+        print("\n CTRL-C pressed , thanks for executing functions_datatypes_1.py script")
+    except:
+        print("\n Input Variables are not numbers, please try again")
+
+    print("Exit, thanks for executing functions_datatypes_1.py script")
+
+if __name__ == "__main__":
+    main()
---
+++ workshops/first/functions_datatypes_2.py
@@ -0,0 +1,44 @@
+def string_reverse(string):
+    """
+    Recursively reverses a string.
+
+    Args:
+        string (str): The string to be reversed.
+
+    Returns:
+        str: The reversed string.
+    """
+    if string:  # If the string has characters, reverse it
+        return string[-1] + string_reverse(string[:-1])
+    else:  # If the string is empty, return an empty string
+        return ""
+
+def main():
+    try:
+
+        flag = False
+        while not flag:
+            print("-------")
+            print("Welcome Main Menu")
+            print("Reverse String Method -> (1)")
+            print("Exit -> (2)")
+            user_input = input("Please choose a mode > ")
+
+            if user_input == "1":  # Reverse your current string
+
+                user_input_value = input("Please enter the string you want to reverse > ")
+
+                print(f"For the original string {user_input_value} the reverse output is {string_reverse(user_input_value)}")
+
+            elif user_input == "2": #Exit
+                flag = True
+
+    except KeyboardInterrupt as error :
+        print("\n CTRL-C pressed , thanks for executing functions_datatypes_2.py script")
+    except:
+        print("\n Input Variables are not numbers, please try again")
+
+    print("Exit, thanks for executing functions_datatypes_2.py script")
+
+if __name__ == "__main__":
+    main()
